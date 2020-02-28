a3c65635dbab8aeabd13936f493a71963e52ce41
Tara Trody <shantara.trody@gmail.com>
2020-02-28 00:10:32 -0500

    update folder structure

--- src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  port: process.env.PORT,
-  db_URL: process.env.MONGO_URL,
-  port_URL: process.env.PORT_URL,
-};
--- src/user/user.router.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { userSignUp } from './user.validation';
-import * as userController from './user.controllers';
-
-export const userRouter = express.Router();
-
-userRouter.post('/signup', userSignUp.validate, userController.signUp);
