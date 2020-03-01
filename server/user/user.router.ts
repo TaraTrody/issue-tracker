59299cb04ef4642b4ac219817fdb527829ffddb3
Tara Trody <shantara.trody@gmail.com>
2020-03-01 01:22:09 -0500

    add logout route

--- server/user/user.router.ts
+++ server/user/user.router.ts
@@ -7,3 +7,5 @@ export const userRouter = express.Router();
 userRouter.post('/signup', userSignUp.validate, userController.signUp);
 
 userRouter.post('/signin', userController.signIn )
+
+userRouter.get('/logout', userController.signOut)
