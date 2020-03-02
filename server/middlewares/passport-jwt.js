2a46e7156e11d12c3188f99c0762d607b9faef98
Tara Trody <shantara.trody@gmail.com>
2020-03-01 21:28:44 -0500

    implement passport JWt strategy

--- server/middlewares/passport-jwt.js
+++ server/middlewares/passport-jwt.js
@@ -1,6 +1,8 @@
 import JWTStrategy from ('passport-jwt').Strategy;
 import ExtractJwt from ('passport-jwt').ExtractJwt;
+import passport from 'passport'
 import config from '../config'
+import User from '../user/user.schema'
 
 let options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -8,8 +10,13 @@ options.secretOrKey = config.jwtSecret
 
 passport.use(new JwtStrategy(options, async function(jwt_payload, done) {
   try {
-    
+    const user = await User.findOne({id: jwt_payload.id})
+    if (user) {
+      return done(null, user)
+    } else {
+      return done(null, false, {message: 'Access Denied'})
+    }
    } catch(err) {
-    throw err
+    return done(err, false)
    }
 })) 
