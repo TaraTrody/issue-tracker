d2d14c4109423cb8ba77b57086fdc3e8350e378c
Tara Trody <shantara.trody@gmail.com>
2020-03-01 21:42:58 -0500

    comment out passport temporarily

--- server/user/user.router.ts
+++ server/user/user.router.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import {Router} from 'express';
 import { userSignUp } from './user.validation';
 import * as userController from './user.controllers';
+// import passport from 'passport'
 
-export const userRouter = express.Router();
+export const userRouter = Router();
 
-userRouter.post('/signup', userSignUp.validate, userController.signUp);
+  userRouter.post('/signup', userSignUp.validate, userController.signUp);
 
-userRouter.post('/signin', userController.signIn )
+  userRouter.post('/signin', userController.signIn )
 
-userRouter.get('/logout', userController.signOut)
+  userRouter.get('/logout', userController.signOut)
+
+
+
+
+// userRouter.get('/test', passport.authenticate('jwt', {session: false}, userController.test))
