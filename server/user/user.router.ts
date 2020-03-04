72f8f7e3904bff3d0a88aba53bbb3666d929e477
Tara Trody <shantara.trody@gmail.com>
2020-03-03 19:50:31 -0500

    clean up file

--- server/user/user.router.ts
+++ server/user/user.router.ts
@@ -2,16 +2,14 @@ import {Router} from 'express';
 import { userSignUp } from './user.validation';
 import * as userController from './user.controllers';
 import passport from 'passport'
+import auth from '../middlewares/passport-local'
 
 export const userRouter = Router();
 
-  userRouter.post('/signup', userSignUp.validate, userController.signUp);
+userRouter.post('/signup', userSignUp.validate, userController.signUp);
 
-  userRouter.post('/signin', passport.authenticate('local', {successRedirect: '/'}), userController.signIn )
+userRouter.post('/login', userController.logIn)
 
-  userRouter.get('/logout', userController.signOut)
+userRouter.get('/logout', userController.logOut)
 
-
-
-
-// userRouter.get('/test', passport.authenticate('jwt', {session: false}, userController.test))
+userRouter.get('/test', auth.isAuth, userController.test)
