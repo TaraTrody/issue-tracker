a2ec2224ac3f58e58f4221e99930450bd16ce862
Tara Trody <shantara.trody@gmail.com>
2020-03-08 23:34:28 -0400

    add Navbar and Landing component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/layout/Landing'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <Navbar />
+      <Landing />
     </div>
   );
 }
