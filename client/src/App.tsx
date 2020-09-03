9f6f577a982e4f628b27759bad7f982a93f47b80
Tara Trody <shantara.trody@gmail.com>
2020-09-03 13:39:13 -0400

    implement react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+
 import Landing from './components/layout/Landing'
 import Navbar from './components/layout/Navbar'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 function App() {
   return (
+  
     <div className="App">
-      <Navbar />
-      <Landing />
+    <Navbar/>
+    <Route exact path="/" component={Landing}/>
+    <Route exact path="/register" component={Register}/>
+    <Route exact path="/login" component={Login}/>
+    
     </div>
   );
 }
