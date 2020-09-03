493b284b832f2c9c1f9989ea33047cdb5a41b285
Tara Trody <shantara.trody@gmail.com>
2020-09-03 13:37:19 -0400

    create Login and Registration components

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,60 +1,37 @@
-import React , {Component, useState} from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
 
-interface FormInputs {
-  name: string;
-  email: string;
-  password: string;
-  errors: {};
+
+const Register =(props) => {
+ const [userInputs, setUserInputs] = useState({firstName: '', lastName: '', email: '', password: ''})
+ const [submitted, setSubmitted] = useState(false)
+
+ handleFormChange = (e) => {
+   const {name, value} = e.target;
+   
+   setUserInputs(state => ({
+     ...state, 
+     [name]: value,
+   }))
+ }
+
+ handleSumbit = (e) => {
+   e.preventDefault()
+
+   setSubmitted(true)
+
+   if (Object.keys(userInputs).every(k => userInputs[k] === true)) {
+     //this.props.register(userInputs) --- must create register Action
+   }
+
+ }
+
+ return (
+   <div className="col-md-6 col-md-offset-3"> 
+        {/* why are my tabs messed up  */}
+   </div> 
+
+ )
+  
 }
 
-
-class Register extends Component<{}, any> {
-  constructor () {
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      errors: {}
-    }
-  }
-
-  const onChange = e => {
-    this.setState({[e.target.id]: e.target.value})
-  }
-
-  onSubmit = e => {
-    e.preventDefault();
-  
-    const newUser = {
-    name: this.state.name,
-    email: this.state.email,
-    password: this.state.password,
-    }
-  console.log(newUser)
-  }
-  render() {
-    const {errors} = this.state;
-    return (
-      <div className="container">
-        <div className="row">
-          <Link to='/' className="btn-flat waves-effect">
-            <i className="material-icons left">keyboard_backspace</i> Back to Home
-          </Link>
-          <div className="col s12"
-              style={{paddingLeft: "11.250px"}}
-          >
-            <h4>
-              <b>Register</b> below
-            </h4>
-            <p className="grey-text text-darken-1">
-              Already have an account?
-            </p>
-
-          </div>
-        </div>
-      </div>
-    )
-  }
-};
+export default Register;
