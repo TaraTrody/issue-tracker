66580132bd4d9bdb00d7ed6280da125ec3d16a49
Tara Trody <shantara.trody@gmail.com>
2020-09-11 18:03:43 -0400

    implement action creators

--- client/src/state/users/auth.actions.js
+++ client/src/state/users/auth.actions.js
@@ -4,3 +4,24 @@ export const USER = '[USER]';
 export const SIGNUP_USER = `${USER} SIGNUP`
 export const LOGIN_USER = `${USER} LOGIN`
 export const SET_USER = `${USER} SET`
+
+export const userSignup = ({user}) => (
+  {
+    type: SIGNUP_USER,
+    payload: user,
+  }
+)
+
+export const userLogin = ({email, password}) => (
+  {
+    type: LOGIN_USER,
+    payload: {email, password}
+  }
+)
+
+export const setUser = ({user}) => (
+  {
+    type: SET_USER,
+    payload: user
+  }
+)
