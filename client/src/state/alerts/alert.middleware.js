a266070c68a51382191b0986f1ccc0c0933f8040
Tara Trody <shantara.trody@gmail.com>
2020-09-10 21:41:18 -0400

    implement clear alert dispatch

--- client/src/state/alerts/alert.middleware.js
+++ client/src/state/alerts/alert.middleware.js
@@ -1,4 +1,27 @@
-export const alertMiddleware = () => (next) => (action) => {
-  next(action)
+import {
+  SET_ALERT,
+  setAlert,
+  clearAlert
+} from './alert.actions'
 
+export const alertMiddleware = () => (next) => (action) => {
+  if (action.type.includes(SET_ALERT)) {
+    const {payload, meta} = action;
+    const id              = new Date().getMilliseconds();
+   
+    const alert = {
+     id,
+     message: payload
+   }
+
+   next(setAlert({message: alert, feature: meta.feature}))
+   
+   setInterval(() => {
+     next(clearAlert({alertId: id, feature: meta.feature}))
+   }, 1000)
+
+  } else {
+    next(action)
+  }
+  
 }
