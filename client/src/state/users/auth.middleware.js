f6b726fee30fd984a7eb1eacbafc20b43c984383
Tara Trody <shantara.trody@gmail.com>
2020-09-11 20:25:35 -0400

    implement user auth middleware

---
+++ client/src/state/users/auth.middleware.js
@@ -0,0 +1,23 @@
+import {USER, SIGNUP_USER, LOGIN_USER, setUser} from './auth.actions'
+import {API_ERROR, API_SUCCESS, apiRequest} from '../api/api.actions'
+import {setAlert} from '../alerts/alert.actions'
+
+
+export const userAuthMiddleware = () => (next) => (action) => {
+  next(action)
+
+  switch (action.type) {
+
+    case SIGNUP_USER: 
+      next(apiRequest({data: action.payload, method: 'POST', url:'/api/v1/user/signup', feature: USER}))
+      break;
+    
+    case `${USER} ${API_SUCCESS}`:
+      next(setUser({user: action.payload}))
+      break;
+
+    case `${USER} ${API_ERROR}`:
+      next(setAlert({error: action.payload, feature: USER}))
+      break; 
+  }
+}
