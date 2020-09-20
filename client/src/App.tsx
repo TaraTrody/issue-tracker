4298666775ea139fa62e5feb1d86738645beea26
Tara Trody <shantara.trody@gmail.com>
2020-09-20 00:55:48 -0400

    clean up the root level

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { SignupLogin } from './features/Signup-Login/';
 import './App.css';
-
-import Landing from './components/layout/Landing'
-import Navbar from './components/layout/Navbar'
-import Register from './components/auth/Register'
-import Login from './components/auth/Login'
 
 function App() {
   return (
-  
     <div className="App">
-    <Navbar/>
-    <Route exact path="/" component={Landing}/>
-    <Route exact path="/register" component={Register}/>
-    <Route exact path="/login" component={Login}/>
-    
+      <SignupLogin />
     </div>
   );
 }
 
 export default App;
+
