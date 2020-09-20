8161c18f4eb7228ff771ff3bf10b43d138708cc8
Tara Trody <shantara.trody@gmail.com>
2020-09-20 18:25:26 -0400

    Create login form component

--- client/src/features/Signup-Login/LoginForm.tsx
+++ client/src/features/Signup-Login/LoginForm.tsx
@@ -1,3 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const LoginForm = () => { }
+const LoginForm = () => { 
+  return (
+    <Container>
+      <Form>
+        <Header></Header>
+
+      </Form>
+    </Container>
+  )
+
+}
+
+export default LoginForm;
+
+const Container = styled.div`
+  width: 24rem;
+  display: flex;
+  
+`;
+
+const Form = styled.form`
+
+`;
+
+const Header = styled.h2``;
+
